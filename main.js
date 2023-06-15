5faf2e36dc3085065d3a3686e908ee9a83e19229
parsachow <parsakishwa06@gmail.com>
2023-06-15 13:56:45 -0400

    updated setup

---
+++ main.js
@@ -0,0 +1,14 @@
+/*----- constants -----*/
+const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
